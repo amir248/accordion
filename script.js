fcd72ed17928fe3dbc07848f7117ee2331f896a1
chikchicly <chikchicly@gmail.com>
2022-08-06 11:40:05 +0700

    oK

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("oK");
+for(let oJ=0;oJ<document.querySelectorAll('img').length;oJ++){
+  console.log(document.querySelectorAll('img')[oJ]);
+
+document.querySelectorAll('img')[oJ].onclick=()=>{
+  newImg();
+    this.document.querySelectorAll('img')[oJ].classList.add('onCl');
+    this.document.querySelectorAll('img')[oJ].classList.remove('onOff');
+  }
+}
+function newImg(){
+  for(let oJ=0;oJ<document.querySelectorAll('img').length;oJ++){
+    document.querySelectorAll('img')[oJ].classList.remove('onCl');
+    document.querySelectorAll('img')[oJ].classList.add('onOff');
+  }
+}
+console.log("oK")
+if(window.innerWidth>=1908){
+  console.log('>1908')
+  let width="3%";
+}else{
+  console.log('<1900');
+}
